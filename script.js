b7846278a6b8fb75e7d60b05517ad99fb21b4cd6
syafaatsaat <47024629+syafaatsaat@users.noreply.github.com>
2026-01-24 09:33:55 +0800

    Wrap GameBoard and Cell factory f inside GameContr
    GameController

--- script.js
+++ script.js
@@ -1,47 +1,52 @@
-function Cell() {
-    let value = "";
+const GameController = (function() {
+    function Cell() {
+        let value = " ";
 
-    const addToken = (token) => {
-        value = token;
-    };
+        const addToken = (token) => {
+            value = token;
+        };
 
-    const getValue = () => value;
+        const getValue = () => value;
 
-    return {
-        addToken,
-        getValue
-    };
-}
-
-const GameBoard = (function() {
-    const rows = 3;
-    const columns = 3;
-    const board = [];
-
-    for (let i = 0; i < rows; ++i) {
-        board[i] = [];
-        for (let j = 0; j < columns; ++j) {
-            board[i].push(Cell());
-        }
+        return {
+            addToken,
+            getValue
+        };
     }
 
-    const getBoard = () => board;
+    function GameBoard() {
+        const rows = 3;
+        const columns = 3;
+        const board = [];
 
-    const chooseCell = (row, column, player) => {
+        for (let i = 0; i < rows; ++i) {
+            board[i] = [];
+            for (let j = 0; j < columns; ++j) {
+                board[i].push(Cell());
+            }
+        }
 
-    };
+        const getBoard = () => board;
 
-    const printBoard = () => {
+        const chooseCell = (row, column, player) => {
+            board[row][column].addToken(player);
+        };
 
-    };
+        const printBoard = () => {
+            const boardWithCellValues = board.map(
+                (row) => row.map(
+                    (cell) => cell.getValue()
+                )
+            );
+            console.log(boardWithCellValues);
+        };
 
-    return {
-        getBoard,
-        chooseCell,
-        printBoard
-    };
-})();
+        return {
+            getBoard,
+            chooseCell,
+            printBoard
+        };
+    }
 
-const GameController = (function() {
     
 })();
